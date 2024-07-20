40b21dcce05f8c34e0b9d666ce57309c1764dc21
chukwu obed <codingwithsmart@gmail.com>
2024-07-20 01:26:42 -1100

    added js code for open menu and scroll animation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 const header = document.querySelector(".header");
 const menuBtb = document.getElementById("menu-btn");
 const updateYear = document.getElementById("year");
+const allLinks = document.querySelectorAll("a:link");
 
 //// Update current year
 
@@ -11,6 +12,33 @@ updateYear.textContent = currentYear;
 
 ///// Toggle menu bar
 
-menuBtb.addEventListener('click', ()=>{
+menuBtb.addEventListener('click', () => {
     header.classList.toggle("open-nav")
 })
+
+allLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const href = link.getAttribute("href");
+
+
+        // add a bottom to top scrol
+
+        if (href === "#") {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            })
+        } else if (href !== "#" && href.startsWith("#")) {
+            const sectionDiv = document.querySelector(href)
+            sectionDiv.scrollIntoView({ behavior: "smooth" })
+        }
+
+        if (link.classList.contains === "main-nav-links") {
+            header.classList.toggle("open-nav")
+        }
+
+    })
+})
+
+
