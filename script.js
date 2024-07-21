c7ec0082980971bbe5f79efa17b891d4a1da4e5a
chukwu obed <codingwithsmart@gmail.com>
2024-07-20 13:55:01 -1100

    added a meta tag for og and twitter

--- script.js
+++ script.js
@@ -16,25 +16,30 @@ menuBtb.addEventListener('click', () => {
     header.classList.toggle("open-nav")
 })
 
+// 
+
 allLinks.forEach((link) => {
     link.addEventListener('click', (e) => {
         e.preventDefault();
         const href = link.getAttribute("href");
 
 
-        // add a bottom to top scrol
+        // add a bottom to top scroll
 
         if (href === "#") {
             window.scrollTo({
                 top: 0,
                 behavior: "smooth",
             })
-        } else if (href !== "#" && href.startsWith("#")) {
+        }
+
+
+        if (href !== "#" && href.startsWith("#")) {
             const sectionDiv = document.querySelector(href)
             sectionDiv.scrollIntoView({ behavior: "smooth" })
         }
 
-        if (link.classList.contains === "main-nav-links") {
+        if (link.classList.contains("main-nav-links")) {
             header.classList.toggle("open-nav")
         }
 
